7fcd006c6954c59d7264dbd5f21e05e9fea8e75c
Niazhub <106829616+Niazhub@users.noreply.github.com>
2023-06-18 17:51:44 +0300

    5 спринт. Итоговый проект(Исправление_3)

---
+++ search-server/read_input_functions.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+#include "search_server.h"
+
+using namespace std;
+
+string ReadLine();
+
+int ReadLineWithNumber();
