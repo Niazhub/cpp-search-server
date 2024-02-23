33b77ceab854009cd5e7228cd63b20298a14f354
Niazhub <106829616+Niazhub@users.noreply.github.com>
2024-02-23 11:20:13 +0300

    Update test_example_functions.h

--- search-server/test_example_functions.h
+++ search-server/test_example_functions.h
@@ -4,33 +4,24 @@
 #include <string>
 #include "search_server.h"
 
-// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов 
 void TestExcludeStopWordsFromAddedDocumentContent();
 
-//Тест проверяет корректное добавление документов 
 void TestAddDocuments();
 
-//Тест проверяет корректную обработку минус-слов 
 void TestMinusWords();
 
-//Тест проверяет корректную сортировку релевантности 
 void TestSortRelevanceDocuments();
 
-//Тест проверяет корректную обработку рейтинга 
 void TestRatings();
 
-//Тест проверяет корректную работу метода MatchDocuments() 
 void TestMatchDocuments();
 
-//Тест проверяет корректность релевантности 
 void TestRelevanceDocuments();
 
-//Проверяет корректную работу предиката 
 void TestFunctionPredicateFilter();
 
 void TestStatusFilter();
 
-//Вызов всех тестов 
 void TestSearchServer();
 
 template <typename T>
